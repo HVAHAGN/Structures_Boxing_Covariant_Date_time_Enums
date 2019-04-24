498de0fb149bd6c73d2db9f844315ced2e56036a
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 22:50:06 +0400

    Можно использовать псевдоним для любого целого типа данных C# (byte, sbyte, short, ushort, int, uint, long, ulong)

---
+++ Enums_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_03
+{
+    // Можно использовать псевдоним для любого целого типа данных C# (byte, sbyte, short, ushort, int,
+    // uint, long, ulong)
+
+    // Нельзя использовать любой системный целый тип данных C# (Byte, SByte, Int16, UInt16, Int32, 
+    // UInt32, Int64, UInt64)
+
+    enum EnumType //: Int32 // Ошибка.
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(EnumType.One);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
