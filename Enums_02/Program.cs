8cc9771bfd8bd4f3ffa7b9b21a0a7d7ca24d310b
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 22:44:21 +0400

    Все перечисления в C# происходят от единого Базового класса System.Enum

---
+++ Enums_02/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_02
+{
+    // Перечисления.
+
+    // Все перечисления в C# происходят от единого Базового класса System.Enum
+    enum EnumType : byte
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Enum one = EnumType.One;
+            Console.WriteLine(one);
+
+            EnumType digit = EnumType.Zero;
+            Enum zero = digit;
+            Console.WriteLine(zero);
+
+            Console.WriteLine(new string('-', 10));
+
+            for (EnumType number = EnumType.Zero; number <= EnumType.Three; number++)
+            {
+                Console.WriteLine(number);
+            }
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
