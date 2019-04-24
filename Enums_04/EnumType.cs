4a887b107f215963f8f6b1fba68cf1a40063e33a
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 22:55:01 +0400

    Enum, как аргумент метода

---
+++ Enums_04/EnumType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_04
+{
+    enum EnumType
+    {
+        Zero,  // = 0 
+        One,   // = 1
+        one = One,
+        Two = 2,
+        Three, // = 3
+        Four,  // = 4
+        Five = 5,
+        Six = Nine,
+        //Seven,
+        Eight = 8, // Если не указать значение явно, то на 13 строке будет ошибка!
+        Nine,
+        Ten = 10,
+        Infinite = 255
+    }
+}
