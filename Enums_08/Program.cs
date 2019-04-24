b3e836c627db126bd25ffc708ebb3143082828df
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 23:29:56 +0400

    Поиск элемента перечисления по имени константы.

---
+++ Enums_08/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_08
+{
+    class Program
+    {
+        // Поиск элемента перечисления по имени константы.
+
+        static void Main()
+        {
+            // Находим элемент перечисления по имени константы.
+            object element = Enum.Parse(typeof(EnumType), "Infinite");
+            EnumType number = (EnumType)element;
+
+            Console.WriteLine("Значение константы {0}: {1}", number, (byte)number);
+
+            // Enum.IsDefined() - Позволяет определить, является ли символьная строка элементом перечисления? 
+            bool flag = Enum.IsDefined(typeof(EnumType), "one");
+
+            if (flag == true)
+                Console.WriteLine("Да, перечисление содержит элемент с таким именем.");
+            else
+                Console.WriteLine("Нет, перечисление не содержит элемент с таким именем.");
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
