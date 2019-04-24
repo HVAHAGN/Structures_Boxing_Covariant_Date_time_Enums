553f314156bc26bffc46070447936f272c2ff2ae
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 17:07:55 +0400

    Add project files.

---
+++ Structures_Boxing_Covariant_Date_time_Enums/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures_Boxing_Covariant_Date_time_Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
