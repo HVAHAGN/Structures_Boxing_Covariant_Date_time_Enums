bed2a22abab6e07d2eee56685a35bc05da4e35a2
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 19:12:35 +0400

    Перечисление - это набор именованных констант, которые хранят числовые значения.

---
+++ Enums_01/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_01
+{
+    // Перечисления.
+
+    // Перечисление - это набор именованных констант, которые хранят числовые значения.
+    // Перечисление определяет именованные константы, каждой из которых соответствует числовое значение.
+    // Все перечисления в C# происходят от единого Базового класса System.Enum
+
+    // При компиляции - компилятор подставляет вместо имен,
+    // установленные им в соответствие числовые значения. [имя] = [число]
+    // По умолчанию типом данных констант перечисления будет int.
+    // Можно использовать любой целый тип данных C# (byte, sbyte, short, ushort, int, uint, long, ulong)
+
+    enum EnumType : byte // Явно указываем использовать тип byte.
+    {
+        Zero = 0,
+        One = 1,
+        Two = 2,
+        Three = 3
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(EnumType.One);
+            Console.WriteLine((byte)EnumType.One);
+
+            EnumType digit = EnumType.Zero;
+            Console.WriteLine(digit);
+            Console.WriteLine((byte)digit);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
