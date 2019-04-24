93dbf3976f4a8c099d7eac98f37796a84b98ad93
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 17:55:33 +0400

    Упаковка и распаковка.

---
+++ Boxing_02/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_02
+{
+    // Упаковка и распаковка.
+    interface IInterface
+    {
+        void Method();
+    }
+
+    struct MyStruct : IInterface
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyStruct my;
+            my.Method();
+            // Упаковка (Boxing).
+            IInterface boxed = my;
+
+            boxed.Method();
+
+            // Распаковка объекта (UnBoxing).
+            MyStruct unBoxed = (MyStruct)boxed;
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
