91bc84199320074a8371b8d9859a846317bce86b
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 23:33:59 +0400

    Сравнение элементов перечисления

---
+++ Enums_10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums_10
+{
+    class Program
+    {
+        // Сравнение элементов перечисления
+
+        static void Main()
+        {
+            EnumType x = EnumType.Five;
+            EnumType y = EnumType.One;
+
+            //if (x < y)                      // Первый вариант сравнения.
+            if (EnumType.Five < EnumType.One) // Второй вариант сравнения.
+                Console.WriteLine("X = {0} (меньше чем) Y = {1}.", x, y);
+            else
+                Console.WriteLine("Y = {0} (меньше чем) X = {1}.", y, x);
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
