18a31a2287f6a4df1995d890d093e458bc3452ab
Vahagn Hovhannisyan <vhoagamo@gmail.com>
2019-04-24 18:11:57 +0400

    Упаковка (Boxing).

---
+++ Boxing_03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boxing_03
+{
+    //Упаковка и распаковка
+
+    struct MyStruct //: ValueType
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            MyStruct my = new MyStruct();
+
+            // Упаковка (Boxing).
+            ValueType boxed = my;
+
+            // Распаковка объекта (UnBoxing).
+            MyStruct unBoxed = (MyStruct)boxed;
+
+            // Delay.
+            Console.ReadKey();
+        }
+    }
+}
